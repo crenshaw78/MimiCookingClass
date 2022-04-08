122d353be2396d95a25079e26416455c05d95e94
Hammurabi <koberstine@hotmail.com>
2022-04-07 20:16:03 -0400

    social media icon placement

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,10 +15,10 @@ function About() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </Col>
       <Col sm={3} id='selfPhoto'>
-          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"> <FaFacebook size={'4vmin'}  style={{background: 'white'}}/> </a>
-          <a href="https://twitter.com/" target="_blank" rel="noreferrer"> <FaTwitter size={'4vmin'}  style={{background: 'white'}}/> </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"> <FaInstagram color='white' size={'4vmin'} style={{background: 'purple'}} /> </a>
-          <a href="https://www.pinterest.com/" target="_blank" rel="noreferrer"> <FaPinterest color='red' size={'4vmin'}  style={{background: 'white'}}/> </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"> <FaFacebook size={'5vmin'}  style={{background: 'white'}}/> </a>
+          <a href="https://twitter.com/" target="_blank" rel="noreferrer"> <FaTwitter size={'5vmin'}  style={{background: 'white'}}/> </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"> <FaInstagram color='white' size={'5vmin'} style={{background: 'purple'}} /> </a>
+          <a href="https://www.pinterest.com/" target="_blank" rel="noreferrer"> <FaPinterest color='red' size={'5vmin'}  style={{background: 'white'}}/> </a>
       </Col>
     </Row>
   );
